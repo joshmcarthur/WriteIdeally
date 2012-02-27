9a53bc5c300d05a3883dda7710a20360f6f60e36
Josh McArthur <joshua.mcarthur@gmail.com>
2012-02-27 23:37:36 +1300

    Set action mailer URL options in testing environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,4 +32,6 @@ Writeideally::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.action_mailer.default_url_options = {:host => "http://localhost:3000"}
 end
