bbd016dfa9829813d2c7689b5b54200aebcd89fd
Josh McArthur <joshua.mcarthur@gmail.com>
2012-10-22 12:31:57 +1300

    Remove existing application files

--- app/models/admin_user.rb
+++
@@ -1,9 +0,0 @@
-class AdminUser < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :oauthable
-  devise :database_authenticatable, :trackable, :timeoutable, :lockable
-
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-end
-
--- lib/ckeditor/ckeditor-3.4.3.pre/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  match 'ckeditor/images', :to => 'ckeditor#images'
-  match 'ckeditor/files',  :to => 'ckeditor#files'
-  match 'ckeditor/create/:kind', :to => 'ckeditor#create'
-end
