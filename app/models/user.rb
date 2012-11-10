097b367c3b32a61f571bc47bef7d91d39c3de00a
Josh McArthur <joshua.mcarthur@gmail.com>
2012-11-10 13:59:47 +1300

    Devise for users

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable,
+         :recoverable, :trackable, :validatable, :confirmable,
+         :authentication_keys => [:login]
+
+  # Define virtual attribute that can contain either an email
+  # or a username while signing in
+  attr_accessor :login
+
+  validates_presence_of :username
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :login, :email, :password, :password_confirmation
+
+  # From:
+  # https://github.com/plataformatec/devise/wiki/How-To:-Allow-users-to-sign-in-using-their-username-or-email-address
+  def self.find_first_by_auth_conditions(warden_conditions)
+  	conditions = warden_conditions.dup
+  	if login = conditions.delete(:login)
+  		where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  	else
+  		where(conditions).first
+  	end
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe User do
+  subject do
+    FactoryGirl.build(:user)
+  end
+
+  it "should be configured for devise" do
+    subject.class.included_modules.should include Devise::Models::Authenticatable
+  end
+
+  it "should have a virtual attribute defined for login" do
+    subject.should respond_to :login
+  end
+
+  it { should validate_presence_of(:username) }
+end
